0cbc97bab8802aebcf895c03b0c756f556f492ea
vipthucthuc <120010844+vipthucthuc@users.noreply.github.com>
2022-12-07 23:32:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
