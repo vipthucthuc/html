2b8822d4465120e54fb832afb6b021f41fbe0897
vipthucthuc <120010844+vipthucthuc@users.noreply.github.com>
2022-12-07 23:38:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/vendor/css/main.css" rel="stylesheet" type="text/css">
+  <link href="/vendor/giaodien.css" rel="stylesheet" type="text/css">
+  <script src="/vendor/jquery/jquery.min.js"></script>
