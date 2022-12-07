77c65e70676bfa2b0a0abccc47efbdf71ba11772
vipthucthuc <120010844+vipthucthuc@users.noreply.github.com>
2022-12-07 23:52:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,3 +19,4 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js"
     integrity="sha512-UwcC/iaz5ziHX7V6LjSKaXgCuRRqbTp1QHpbOJ4l1nw2/boCfZ2KlFIqBUA/uRVF0onbREnY9do8rM/uT/ilqw=="
     crossorigin="anonymous"></script>
+  <script src="/Models/VipttWS.js?request=<?=time()?>"></script>
