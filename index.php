b515d68ee7573d9493ff2a4c1cae651c709d85b1
vipthucthuc <120010844+vipthucthuc@users.noreply.github.com>
2022-12-07 23:50:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,15 @@
   <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="/vendor/css/main.css" rel="stylesheet" type="text/css">
   <link href="/vendor/giaodien.css" rel="stylesheet" type="text/css">
+  <link href="/vendor/v2/css.css?<?=time()?>" rel="stylesheet">
+  <link href="/vendor/v2/d.scss?<?=time()?>" rel="stylesheet">
+
   <script src="/vendor/jquery/jquery.min.js"></script>
+  <script src="/Models/WebSocket.js?request=<?=time()?>"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"
+    integrity="sha512-efUTj3HdSPwWJ9gjfGR71X9cvsrthIA78/Fvd/IN+fttQVy7XWkOAXb295j8B3cmm/kFKVxjiNYzKw9IQJHIuQ=="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js"
+    integrity="sha512-UwcC/iaz5ziHX7V6LjSKaXgCuRRqbTp1QHpbOJ4l1nw2/boCfZ2KlFIqBUA/uRVF0onbREnY9do8rM/uT/ilqw=="
+    crossorigin="anonymous"></script>
